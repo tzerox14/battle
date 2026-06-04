3257a8502acd2cd849512dae5561b40fcd2f8dab
tzerox14 <105602905+tzerox14@users.noreply.github.com>
2026-06-04 21:02:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -613,7 +613,7 @@ wss.on('connection',ws=>{
   });
 });
 
-const PORT=process.env.PORT||58700;
+const PORT=process.env.PORT||8080;
 server.listen(PORT,'0.0.0.0',()=>{
   const{networkInterfaces}=require('os');
   const nets=networkInterfaces();let ip='localhost';
